45922c497f6e03cdefa4dfbea6573ec1f76340f9
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-11 23:33:19 +0300

    messages

--- tgBot/test.pl
+++ tgBot/test.pl
@@ -10,6 +10,7 @@ print "flag 1";
 
 #хеш в которые будет добавлять пару ключ/значение  уникального пользователя, проверка гита
 my %users;
+my %messages;
  
 my $token = '6066175785:AAGbPy6vKuuneCAP8XI7XC8fJAl80nfeAfQ';
 my $chat_id = '-1001912609456';
@@ -20,10 +21,18 @@ my $number_of_update = 0;
 
 $bot->call('sendMessage', { chat_id => $chat_id, text => "Запуск программы." });
 
+open(F0,"< one.txt");
+my @strings=<F0>;
+foreach my $str (@strings)
+{
+    chomp $str;
+    $messages{$str} = 1;
+}
+close(F0);
+
 while (1)
 {
     my $updates = $bot->call('getUpdates', { offset => $number_of_update + 1 } );
-    print "/\n";
 
     foreach my $update ( @{$updates->{result}} )
     {
@@ -35,14 +44,28 @@ while (1)
         {
             my $user_of_message_id = $upd_message->{ from }->{ id };
             my $user_of_message_name = $upd_message->{ from }->{ first_name };
+            my $text_of_message = $upd_message->{ text };
 
-            $bot->call('sendMessage', { chat_id => $chat_id, text =>  $user_of_message_name});        
+            if($messages {$text_of_message} == 1)
+            {
+                print "ok";
+            }
+            else
+            {
+                print "NO";
+                $messages{$text_of_message} = 1;
+                $bot->call('sendMessage', { chat_id => $chat_id, text =>  $text_of_message});
+                open(FH, ">> one.txt");
+                print (FH $text_of_message . "\n");
+                close (FH);
+            }
 
-            # if ( $message->{ new_chat_members })
+            # if ( $upd_message->{ new_chat_members })
             # {
-            #     foreach my $new_member ( @{$message->{ new_chat_members }} )
+            #     foreach my $new_member ( @{$upd_message->{ new_chat_members }} )
             #     {
             #         my $new_member_id = $new_member->{ id }; #
+            #         $users{ $new_member_id } = 0; #
             #     }
             # }
         }
