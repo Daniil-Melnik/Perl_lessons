65ed97d7e01c622587678b6b95401e9ad2db5783
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-11 23:55:36 +0300

    Update test.pl

--- tgBot/test.pl
+++ tgBot/test.pl
@@ -6,6 +6,8 @@ use FindBin;
 use lib $FindBin::Bin;
 use WebProgTelegramClient;
 
+use Time::Local;
+
 print "flag 1";
 
 #хеш в которые будет добавлять пару ключ/значение  уникального пользователя, проверка гита
@@ -21,14 +23,14 @@ my $number_of_update = 0;
 
 $bot->call('sendMessage', { chat_id => $chat_id, text => "Запуск программы." });
 
-open(F0,"< one.txt");
-my @strings=<F0>;
-foreach my $str (@strings)
-{
-    chomp $str;
-    $messages{$str} = 1;
-}
-close(F0);
+# open(F0,"< one.txt");
+# my @strings=<F0>;
+# foreach my $str (@strings)
+# {
+#     chomp $str;
+#     $messages{$str} = 1;
+# }
+# close(F0);
 
 while (1)
 {
@@ -45,42 +47,29 @@ while (1)
             my $user_of_message_id = $upd_message->{ from }->{ id };
             my $user_of_message_name = $upd_message->{ from }->{ first_name };
             my $text_of_message = $upd_message->{ text };
+            my $date_of_message = $upd_message->{ date };
 
-            if($messages {$text_of_message} == 1)
-            {
-                print "ok";
-            }
-            else
-            {
-                print "NO";
-                $messages{$text_of_message} = 1;
-                $bot->call('sendMessage', { chat_id => $chat_id, text =>  $text_of_message});
-                open(FH, ">> one.txt");
-                print (FH $text_of_message . "\n");
-                close (FH);
-            }
+            my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($date_of_message);
 
-            # if ( $upd_message->{ new_chat_members })
-            # {
-            #     foreach my $new_member ( @{$upd_message->{ new_chat_members }} )
-            #     {
-            #         my $new_member_id = $new_member->{ id }; #
-            #         $users{ $new_member_id } = 0; #
-            #     }
-            # }
+            if ( $upd_message->{ new_chat_members })
+            {
+                foreach my $new_member ( @{$upd_message->{ new_chat_members }} ) #---
+                {
+                    my $new_member_id = $new_member->{ id }; #
+                    $users{ $new_member_id } = 0; #
+                }
+            }
+            elsif ( $users{ $user_of_message_id } == 0 && $upd_message->{ text } ) 
+            { 
+            # Присваевыем новое значение в хеш
+                $users{ $user_of_message_id } = 1;
+                $bot->call( 'sendMessage', { chat_id => $chat_id, text => "Привет, $user_of_message_name!" } );#
+            }
         }
     }
 }
 
 
-#           foreach my $new_member ( @{$message->{ new_chat_members }} ) 
-#           {
-#             # Получаем id нового участника чата
-#             my $new_member_id = $new_member->{ id };
-#               # добавляем в хеш id  участника с значением "0";
-#               $users{ $new_member_id } = 0;
-#           }
-#         }
 #         # Проверяем в хеше равно ли значение участника - "0", проверка на уникальность сообщения участника
 #         elsif ( $users{ $user_id } == 0 && $message->{ text } ) 
 #         { 
