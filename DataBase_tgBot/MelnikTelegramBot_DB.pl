5f13d0a0776036bf58b7f481bb7c2889004cc4e8
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-14 19:24:02 +0300

    Update MelnikTelegramBot_DB.pl

--- DataBase_tgBot/MelnikTelegramBot_DB.pl
+++ DataBase_tgBot/MelnikTelegramBot_DB.pl
@@ -4,11 +4,23 @@ use utf8;
 use lib "./";
 use WebProgTelegramClient;
 
+use DBI;
+
+my $attr = {PrintError => 0, RaiseError => 0};
+my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
+my $username = "webprog5_melnik";
+my $password = "2WsxcdE3";
+
+my $dbh = DBI->connect($data_source, $username, $password, $attr);
+if (!$dbh) { die $DBI::errstr; }
+$dbh->do('SET NAMES cp1251');
+
+
 # use Time::Local;
 
 # объявление токена бота и id группы для дальнейшей работы
 my $token = '6066175785:AAGbPy6vKuuneCAP8XI7XC8fJAl80nfeAfQ';
-my $chat_id = '-1001929018339';
+my $chat_id = '-1001874774612';
 
 # создание базовых условий: создание бота, поиск чата, задание начального значения id обновления
 my $bot = WebProgTelegramClient->new( token => $token );
@@ -20,53 +32,29 @@ my $number_of_update = 0;
 # $bot->call( 'sendMessage', { chat_id => $chat_id, text => "Запуск программы" } );
 
 # бесконечный цикл для постоянного контроля за изменениями в группе
-while (1)
+
+my $updates = $bot->call( 'getUpdates', { offset => $number_of_update + 1 } );
+my $group_num = 3;
+
+# рассмотрение всех обновлений
+foreach my $update ( @{ $updates->{result} } )
 {
-  my $updates = $bot->call( 'getUpdates', { offset => $number_of_update + 1 } );
-
-  # рассмотрение всех обновлений
-  foreach my $update ( @{ $updates->{result} } )
+  # выделение из обновления сообщения
+  my $upd_message = $update->{ message };
+  if ( $upd_message )
   {
-    # выделение из обновления сообщения
-    my $upd_message = $update->{ message };
-    if ( $upd_message )
-    {
-      # выделение из сообщения основных его характеристик: id и имя пользователя, дату сообщения
-      my $user_of_message_id = $upd_message->{ from }->{ id };
-      my $user_of_message_name = $upd_message->{ from }->{ first_name };
-      # my $date_of_message = $upd_message->{ date };
+    # выделение из сообщения основных его характеристик: id и имя пользователя, дату сообщения
+    my $user_of_message_id = $upd_message->{ from }->{ id };
+    my $user_of_message_name = $upd_message->{ from }->{ first_name };
+    # my $date_of_message = $upd_message->{ date };
 
-      # конвертация даты в обычный формат
-      # my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime( $date_of_message );
+    # конвертация даты в обычный формат
+    # my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime( $date_of_message );
 
-      # контроль появления новых пользователей через спец. хеш
-      if ( $upd_message->{ new_chat_members } ) 
-      {
-        foreach my $new_member ( @{ $upd_message->{ new_chat_members } } ) 
-        {
-          my $new_member_id = $new_member->{id};
-          $all_members{ $new_member_id } = 0;
-        }
-      }
+    my $sth = $dbh->prepare("INSERT INTO student_id(student_name,group_num) VALUES (?,?)");
+    $sth->execute( $user_of_message_name, $group_num );
 
-      elsif ($all_members{ $user_of_message_id } == 0 && $upd_message->{ text }) 
-      { 
-        $all_members{ $user_of_message_id } = 1;
-        my $ans = "Привет, " . $user_of_message_name . "!";;
-        $bot->call( 'sendMessage', { chat_id => $chat_id, text => $ans } );
-      }
-
-      # контроль пользователей, покинувших группу
-      if ( $upd_message->{ left_chat_member } )
-        {
-          my $left_member = $upd_message->{ left_chat_member };
-          if ($left_member && $left_member->{ first_name })
-          {
-            my $left_member_name = $left_member->{ first_name };
-            $bot->call('sendMessage', { chat_id => $chat_id, text => "Желаем удачи, " .  $left_member_name . "."});
-          }
-        }
-    }
-    $number_of_update = $update->{ update_id };
   }
+  $number_of_update = $update->{ update_id };
 }
+
