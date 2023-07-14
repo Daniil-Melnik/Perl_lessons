67f8a44d0f5b02f774f9c1222ea407457ab66f29
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-14 16:27:09 +0300

    Update testDB.pl

--- DataBase_tgBot/testDB.pl
+++ DataBase_tgBot/testDB.pl
@@ -1,9 +1,9 @@
 use DBI;
 
-# Подключение к БД
+
 my $attr = {PrintError => 0, RaiseError => 0};
 my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
-my $username = "melnik";
+my $username = "webprog5_melnik";
 my $password = "2WsxcdE3";
 print "ok0\n";
 my $dbh = DBI->connect($data_source, $username, $password, $attr);
@@ -13,7 +13,7 @@ print "ok2\n";
 $dbh->do('SET NAMES cp1251');
 
 
-# Выполнение запроса к БД
+
 my $rv = $dbh->selectrow_arrayref('SHOW TABLES');
 
 die Dumper($rv);
