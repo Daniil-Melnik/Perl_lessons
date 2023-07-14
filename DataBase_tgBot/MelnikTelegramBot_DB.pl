975121e630a7002dc914bc0a375aaf6a8deffc02
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-15 02:29:33 +0300

    Update MelnikTelegramBot_DB.pl

--- DataBase_tgBot/MelnikTelegramBot_DB.pl
+++ DataBase_tgBot/MelnikTelegramBot_DB.pl
@@ -7,31 +7,29 @@ use WebProgTelegramClient;
 
 use DBI;
 
-my $attr = {PrintError => 0, RaiseError => 0};
+my $attr = { PrintError => 0, RaiseError => 0 };
 my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
 my $username = "webprog5_melnik";
 my $password = "2WsxcdE3";
 
-my $dbh = DBI->connect($data_source, $username, $password, $attr);
+my $dbh = DBI->connect( $data_source, $username, $password, $attr );
 if (!$dbh) { die $DBI::errstr; }
 $dbh->do('SET NAMES cp1251');
 
-
-# use Time::Local;
-
+use Time::Local;
 
 my $token = '6066175785:AAGbPy6vKuuneCAP8XI7XC8fJAl80nfeAfQ';
 my $chat_id = '-1001874774612';
 
 my $bot = WebProgTelegramClient->new( token => $token );
-my $chat = $bot->call( 'getChat', {chat_id => $chat_id} );
+my $chat = $bot->call( 'getChat', { chat_id => $chat_id } );
 
 my %all_members;
-my %deadlines = (1 => "13.07.2023", 2 => "15.07.2023");
-my $sth = $dbh->prepare("INSERT INTO hw_id(hw_num,deadline) VALUES (?,?)");
+my %deadlines = ( 1 => "13.07.2023", 2 => "15.07.2023" );
+my $sth = $dbh->prepare( "INSERT INTO hw_id(hw_num,deadline) VALUES (?, ?)" );
 
-$sth->execute(1, $deadlines{1});
-$sth->execute(2, $deadlines{2});
+$sth->execute( 1, $deadlines{1} );
+$sth->execute( 2, $deadlines{2} );
 
 my $number_of_update = 0;
 
@@ -47,18 +45,18 @@ foreach my $update ( @{ $updates->{result} } )
     my $user_of_message_name = $upd_message->{ from }->{ first_name };
 
   
-    if (!$all_members{$user_of_message_id})
+    if ( !$all_members{ $user_of_message_id } )
     {
-      $sth = $dbh->prepare("INSERT INTO student_id(student_name,group_num) VALUES (?,?)");
+      $sth = $dbh->prepare( "INSERT INTO student_id(student_name, group_num) VALUES (?, ?)" );
       $sth->execute( $user_of_message_name, $group_num );
-      $all_members{$user_of_message_id} = 1;
+      $all_members{ $user_of_message_id } = 1;
     }
   }
   $number_of_update = $update->{ update_id };
 }
 my $arrayref_of_row_hashrefs_students;
 my $arrayref_of_row_hashrefs_hw_ids;
-$arrayref_of_row_hashrefs_students = $dbh->selectall_arrayref("SELECT id, student_name, group_num FROM student_id WHERE group_num BETWEEN ? AND ?",{ Slice => {} }, 0, 5);
+$arrayref_of_row_hashrefs_students = $dbh->selectall_arrayref( "SELECT id, student_name, group_num FROM student_id WHERE group_num BETWEEN ? AND ?", { Slice => {} }, 0, 5 );
 foreach my $update ( @{ $updates->{result} } )
 {
   my $upd_message = $update->{ message };
@@ -73,18 +71,18 @@ foreach my $update ( @{ $updates->{result} } )
     {
       my ($hw_num) = $upd_message->{text} =~ /(\d+)/;
       my ($item) = grep {
-        $_->{student_name} eq $user_of_message_name;
+        $_->{ student_name } eq $user_of_message_name;
       } @$arrayref_of_row_hashrefs_students;
 
-      my $sth = $dbh->prepare("INSERT INTO webprog5_melnik_results(student_id, hw_num, result, date_of_complite) VALUES (?,?,?,?)");
-      my @spl = split(/\./, $deadlines{$hw_num});
+      my $sth = $dbh->prepare( "INSERT INTO webprog5_melnik_results(student_id, hw_num, result, date_of_complite) VALUES (?, ?, ?, ?)" );
+      my @spl = split( /\./, $deadlines{$hw_num} );
       #my $text = "23.22.22";
       #my @spl = split(/\./, $text); 
       my $result;
       #print @spl;
       #print ($deadlines{$hw_num});
       #print $spl[0] . "." . $spl[1] . "." . $spl[2] . "\n";
-      if (($year-1900 <= $spl[2]) && ($mday <= $spl[0]) && ($mon <= $spl[1]+1))
+      if ( ( $year-1900 <= $spl[2] ) && ( $mday <= $spl[0] ) && ( $mon <= $spl[1]+1 ) )
       {
         $result = 10;
       }
@@ -97,5 +95,24 @@ foreach my $update ( @{ $updates->{result} } )
   }
   $number_of_update = $update->{ update_id };
 }
+
+show_table();
+print "\n";
+
 my $sth2 = $dbh->prepare("UPDATE webprog5_melnik_results SET result = ? WHERE id = ?");
-$sth2->execute(50, 73) or die "Unable to update\n";
+$sth2->execute( 8, 73 ) or die "Unable to update\n";
+$sth2->execute( 4, 76 ) or die "Unable to update\n";
+
+show_table();
+
+sub show_table 
+{
+  shift;
+  my $sth = $dbh->prepare("SELECT * FROM webprog5_melnik_results;");
+  $sth->execute();
+  while (my @arr = $sth->fetchrow_array())
+  {
+    print join ("\t", @arr), "\n";
+  }
+  $sth->finish();
+}
