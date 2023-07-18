25daa369dafcf863cd004b3010a8fddb5ed74972
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-18 15:28:15 +0300

    .

--- GUI_WEB_tgBot_DB/melnik_group_list.pl
+++ GUI_WEB_tgBot_DB/melnik_group_list.pl
@@ -1,13 +1,12 @@
 use strict;
 use warnings;
 use utf8;
-use lib "./";
+#use lib "./";
 use HTML::Template;
 
-my $template = HTML::Template->new(filename => 'index.html');
+my $template = HTML::Template->new(filename => 'melnik_group_list.html');
 
-# массив 
-$template->param(employee_info => [{name => 'Sam', job => 'programmer'}, {name => 'Steve', job => 'soda jerk'}]);
+$template->param(info => [{name => 'Sam', job => 'programmer'}, {name => 'Steve', job => 'soda jerk'}]);
 
 print "Content-Type: text/html\n";
 print "Charset: windows-1251\n\n";
