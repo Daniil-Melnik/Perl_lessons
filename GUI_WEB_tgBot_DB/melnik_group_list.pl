c8810e2a2c44a365bdb8f552c3b20f68f4472ff0
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-19 00:12:24 +0300

    .

--- GUI_WEB_tgBot_DB/melnik_group_list.pl
+++ GUI_WEB_tgBot_DB/melnik_group_list.pl
@@ -1,12 +1,24 @@
 #!/usr/bin/perl -w
 use strict;
 use HTML::Template;
+use DBI;
  
 use lib '/home/webprog5/data/www/webprogmelnik.trudogolik.ru/cgi-bin/';
 
+my $attr = { PrintError => 0, RaiseError => 0 };
+my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
+my $username = "webprog5_melnik";
+my $password = "2WsxcdE3";
+
+my $dbh = DBI->connect( $data_source, $username, $password, $attr );
+if (!$dbh) { die $DBI::errstr; }
+$dbh->do('SET NAMES cp1251');
+
+my $arrayref_of_groups = $dbh->selectall_arrayref( "SELECT id, tg_id FROM group_id WHERE id BETWEEN ? AND ?", { Slice => {} }, 0, 5 );
+
 my $template = HTML::Template->new(filename => "melnik_group_list.html");
 
-$template->param(info => [{name => 'Sam', job => 'programmer'}, {name => 'Steve', job => 'soda jerk'}]);
+$template->param(info => $arrayref_of_groups);
 
 print "Content-Type: text/html\n";
 print "Charset: windows-1251\n\n";
