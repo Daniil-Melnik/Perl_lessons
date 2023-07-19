f81ad8c7168af094f8c40a308ab156d87f61611f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-19 23:12:44 +0300

    used sendPhoto

--- autoMafia/MelnikTelegramBot_auto.pl
+++ autoMafia/MelnikTelegramBot_auto.pl
@@ -15,23 +15,24 @@ my %all_members;
 my $number_of_update = 0;
 
 $bot->call( 'sendMessage', { chat_id => $chat_id, text => "start_1" } );
+$bot->call( 'sendPhoto', { chat_id => $chat_id, photo => "https://i.ytimg.com/vi/65zkVM_gWDE/maxresdefault.jpg?7857057827" } );
 
-my $updates = $bot->call( 'getUpdates', { offset => $number_of_update + 1 } );
+# my $updates = $bot->call( 'getUpdates', { offset => $number_of_update + 1 } );
 
-foreach my $update ( @{ $updates->{result} } )
-  {
-    my $upd_message = $update->{ message };
-    if ( $upd_message )
-    {
-      my $user_of_message_id = $upd_message->{ from }->{ id };
-      my $user_of_message_name = $upd_message->{ from }->{ first_name };
-      my $date_of_message = $upd_message->{ date };
+# foreach my $update ( @{ $updates->{result} } )
+#   {
+#     my $upd_message = $update->{ message };
+#     if ( $upd_message )
+#     {
+#       my $user_of_message_id = $upd_message->{ from }->{ id };
+#       my $user_of_message_name = $upd_message->{ from }->{ first_name };
+#       my $date_of_message = $upd_message->{ date };
 
-      my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime( $date_of_message );
-      print ($hour.' '.$min.' '.$sec."\n");
-    }
-    $number_of_update = $update->{ update_id };
-  }
+#       my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime( $date_of_message );
+#       print ($hour.' '.$min.' '.$sec."\n");
+#     }
+#     $number_of_update = $update->{ update_id };
+#   }
 
 print "ok1";
 
