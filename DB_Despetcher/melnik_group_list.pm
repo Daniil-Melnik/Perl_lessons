763ef5f4f41c67410c2d5b2da26c7518860d1f18
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-20 22:31:16 +0300

    .

--- DB_Despetcher/melnik_group_list.pm
+++ DB_Despetcher/melnik_group_list.pm
@@ -1,12 +1,13 @@
 #!/usr/bin/perl -w
 
-package melnik_group_list
+package melnik_group_list;
 
 use strict;
+use warnings;
+
 use HTML::Template;
-use DBI;
- 
-use lib '/home/webprog5/data/www/webprogmelnik.trudogolik.ru/cgi-bin/';
+use DBConnect;
+
 
 sub new
 {
@@ -17,31 +18,25 @@ sub new
     return $self;
 }
 
+
 sub show_list
 {
-    my $cgi = CGI->new;
+  my $cgi = CGISParams->new;
 
-    # my $attr = { PrintError => 0, RaiseError => 0 };
-    # my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
-    # my $username = "webprog5_melnik";
-    # my $password = "2WsxcdE3";
+  my $template = HTML::Template->new( filename => "./melnik_group_list.html" );
 
-    # my $dbh = DBI->connect( $data_source, $username, $password, $attr );
-    # if (!$dbh) { die $DBI::errstr; }
-    # $dbh->do('SET NAMES cp1251');
+  my $link = DBConnect->new();
+  my $dbh = $link->get_dbh();
 
-    my $link = DBConnect->new();
-    my $dbh = $link->get_dbh();
+  my $arrayref_of_groups = $dbh->selectall_arrayref( "SELECT id, tg_id FROM group_id WHERE id BETWEEN ? AND ?", { Slice => {} }, 0, 50 );
 
-    my $arrayref_of_groups = $dbh->selectall_arrayref( "SELECT id, tg_id FROM group_id WHERE id BETWEEN ? AND ?", { Slice => {} }, 0, 50 );
+  $template->param(info => $arrayref_of_groups);
 
-    my $template = HTML::Template->new(filename => "melnik_group_list.html");
-
-    $template->param(info => $arrayref_of_groups);
-
-    print "Content-Type: text/html\n";
-    print "Charset: windows-1251\n\n";
-    print $template->output;
+  print "Content-Type: text/html\n";
+  print "Charset: windows-1251\n\n";
+  # $template->param(class => $cl);
+  # $template->param(event => $event);
+  print $template->output;
 }
 
 1
