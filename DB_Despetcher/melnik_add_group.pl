76049267d65ce8e7127eb5fd2d500f58b3fcf773
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-21 15:22:02 +0300

    Create melnik_add_group.pl

---
+++ DB_Despetcher/melnik_add_group.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+use lib './';
+require 'io_cgi.pm';
+use strict;
+use warnings;
+use DBI;
+use Scalar::Util qw(looks_like_number);
+
+my $io_cgi = 'io_cgi'->new;
+$io_cgi->get_params();
+my $group_id = $io_cgi->param('group_id');
+
+
+
+  my $attr = { PrintError => 0, RaiseError => 1 };
+  my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
+  my $username = "webprog5_melnik";
+  my $password = "2WsxcdE3";
+  my $dbh = DBI->connect( $data_source, $username, $password, $attr ) or die $DBI::errstr;
+
+  my $sth = $dbh->prepare( "INSERT INTO group_id (tg_id) VALUES (?);" );
+  $sth->execute($group_id);
+
+  print "Location: melnik_group_list.pl\n\n"; 
+  print "Content-type: text/html\n\n"; 
+
+
+
