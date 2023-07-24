5afb8650d28aa9538b555404f2d4fd84b4283177
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-24 13:46:24 +0300

    selection /

--- autoMafia/MelnikTelegramBot_auto.pl
+++ autoMafia/MelnikTelegramBot_auto.pl
@@ -14,6 +14,17 @@ my $chat = $bot->call( 'getChat', {chat_id => $chat_id} );
 my %all_members; 
 my $number_of_update = 0;
 
+# my $attr = { PrintError => 0, RaiseError => 0 };
+# my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
+# my $username = "webprog5_melnik";
+# my $password = "2WsxcdE3";
+
+# my $dbh = DBI->connect( $data_source, $username, $password, $attr );
+# if (!$dbh) { die $DBI::errstr; }
+
+# $dbh->do('SET NAMES cp1251');
+# my $arrayref_of_res = $dbh->selectall_arrayref( "SELECT id, student_id, hw_num, result, date_of_complite FROM webprog5_melnik_results", { Slice => {} });
+
 #$bot->call( 'sendMessage', { chat_id => $chat_id, text => "start_1" } );
 #$bot->call( 'sendPhoto', { chat_id => $chat_id, photo => "https://i.ytimg.com/vi/65zkVM_gWDE/maxresdefault.jpg?7857057827" } );
 
@@ -27,9 +38,12 @@ foreach my $update ( @{ $updates->{result} } )
       my $user_of_message_id = $upd_message->{ from }->{ id };
       my $user_of_message_name = $upd_message->{ from }->{ first_name };
       my $date_of_message = $upd_message->{ date };
+      my $message_text = $upd_message->{ text };
 
-      my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime( $date_of_message );
-      print ($hour . ' ' . $min . ' ' . $sec . " " . $upd_message->{ text } . "\n");
+      if (index($message_text, "/") == 0)
+      {
+        print "true\n";
+      } 
     }
     $number_of_update = $update->{ update_id };
   }
