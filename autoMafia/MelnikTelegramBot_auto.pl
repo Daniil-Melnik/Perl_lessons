2fa02206661b6771fc865c9cd7116f395ac5dd7f
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-07-24 23:58:17 +0300

    чушь

--- autoMafia/MelnikTelegramBot_auto.pl
+++ autoMafia/MelnikTelegramBot_auto.pl
@@ -1,12 +1,13 @@
 use strict;
 use warnings;
-use utf8;
 use lib "./";
 use WebProgTelegramClient;
 use DBI;
 
 use Time::Local;
 use DateTime;
+use utf8;
+#use cp1251;
 
 my $token = '6164752146:AAHybXJiUKBnL6f5MRv-Hf7g8i574BW9Te4';
 my $chat_id = '-1001910130358';
@@ -16,26 +17,26 @@ my $chat = $bot->call( 'getChat', {chat_id => $chat_id} );
 my %all_members; 
 my $number_of_update = 0;
 
-my $attr = { PrintError => 0, RaiseError => 0 };
-my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
-my $username = "webprog5_melnik";
-my $password = "2WsxcdE3";
+# my $attr = { PrintError => 0, RaiseError => 0 };
+# my $data_source = "DBI:mysql:webprog5_melniktgbot:localhost";
+# my $username = "webprog5_melnik";
+# my $password = "2WsxcdE3";
 
-my $dbh = DBI->connect( $data_source, $username, $password, $attr );
-if (!$dbh) { die $DBI::errstr; }
+# my $dbh = DBI->connect( $data_source, $username, $password, $attr );
+# if (!$dbh) { die $DBI::errstr; }
 
-$dbh->do('SET NAMES cp1251');
+# $dbh->do('SET NAMES cp1251');
 
 
 #$bot->call( 'sendMessage', { chat_id => $chat_id, text => "/culverempire\n/shubertAB\n/ascotbaileyS200\n/smiththunderbolt\n/shubertbeverly\n/patomac_indian\n/smith_custom_200\n/lassiter_series_75\n/smith_deluxe_station_wagon
 #/walter_military" } );
 #$bot->call( 'sendPhoto', { chat_id => $chat_id, photo => "https://i.ytimg.com/vi/65zkVM_gWDE/maxresdefault.jpg?7857057827" } );
 
-my $time = time()-15;
+my $time = time() - 500;
 #print $time;
 
 my $updates = $bot->call( 'getUpdates', { offset => $number_of_update + 1 } );
-
+$bot->call( 'sendMessage', { chat_id => $chat_id, text => "Привет" } );
 foreach my $update ( @{ $updates->{result} } )
   {
     my $upd_message = $update->{ message };
@@ -48,21 +49,27 @@ foreach my $update ( @{ $updates->{result} } )
 
       if ((index($message_text, "/") == 0)&&(index($message_text, "@") != -1))
       {
-        my @message_command = split('@', $message_text);
-        # print ($date_of_message);
-        my $sql = "SELECT name, url FROM auto WHERE comand = ?";
-        my $sth = $dbh->prepare($sql);
+        if ($date_of_message >= $time)
+        {
+          my @message_command = split('@', $message_text);
+          # print ($date_of_message);
+          # my $sql = "SELECT name, discription, url FROM auto WHERE comand = ?";
+          # my $sth = $dbh->prepare($sql);
 
-        # execute the query
-        $sth->execute($message_command[0]);
+          # # execute the query
+          # $sth->execute($message_command[0]);
 
-        while(my @row = $sth->fetchrow_array()){
-          $bot->call( 'sendPhoto', { chat_id => $chat_id, photo => $row[1] } );
-        }       
-        $sth->finish();
-        
+          # while(my @row = $sth->fetchrow_array()){
+          #   $bot->call( 'sendMessage', { chat_id => $chat_id, text => $row[1] } );
+          #   #$bot->call( 'sendPhoto', { chat_id => $chat_id, photo => $row[2] } );
+          #   #print ($row[0] . " " . "\n" . $row[1] . "\n" . $row[2]);
+          # }
+                 
+          # $sth->finish();
+        }
       } 
     }
+
     $number_of_update = $update->{ update_id };
   }
 
